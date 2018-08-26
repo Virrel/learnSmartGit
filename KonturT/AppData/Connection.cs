e309185bab91f13aa51dfb31244752b5046b9876
Virrel <demismir@gmail.com>
2018-08-26 13:44:13 +0500

    FFirst commit

---
+++ KonturT/AppData/Connection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Data.Sql;
+
+namespace KonturT.AppData
+{
+    class ConnectionsF
+    {
+        SQLiteConnection con = new SQLiteConnection("Data Source=:memory:;Version=3;New=True;");
+        SQLiteCommand cmd = new SQLiteCommand();
+        
+
+        string sql_command = "DROP TABLE IF EXISTS person;"
+          + "CREATE TABLE person("
+          + "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+          + "first_name TEXT, "
+          + "last_name TEXT, "
+          + "sex INTEGER, "
+          + "birth_date INTEGER);"
+          + "INSERT INTO person(first_name, last_name, sex, birth_date) "
+          + "VALUES ('John', 'Doe', 0, strftime('%s', '1979-12-10'));"
+          + "INSERT INTO person(first_name, last_name, sex, birth_date) "
+          + "VALUES ('Vanessa', 'Maison', 1, strftime('%s', '1977-12-10'));"
+          + "INSERT INTO person(first_name, last_name, sex, birth_date) "
+          + "VALUES ('Ivan', 'Vasiliev', 0, strftime('%s', '1987-01-06'));"
+          + "INSERT INTO person(first_name, last_name, sex, birth_date) "
+          + "VALUES ('Kevin', 'Drago', 0, strftime('%s', '1991-06-11'));"
+          + "INSERT INTO person(first_name, last_name, sex, birth_date) "
+          + "VALUES ('Angel', 'Vasco', 1, strftime('%s', '1987-10-09'));";
+    }
+}
---
+++ KonturT/Models/Server.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KonturT.Models
+{
+    public class Server
+    {
+        [JsonIgnore]
+        public int Id { get; set; }
+        public string endpoint { get; set; }
+        //[ForeignKey("infoId")]
+        //[JsonIgnore]
+        public int serverInfoId { get; set; }
+        public ServerInfo info { get; set; }
+        //public virtual IEnumerable<Map> matches { get; set; }
+    }
+}
